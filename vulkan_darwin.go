a3ed9d78bc9e247a28ef1f87cce8622efe560a4f
Tomáš Mráz <tomas.mraz@zona64.cz>
2026-04-08 13:27:47 +0200

    build flags for kosmic krisp

--- vulkan_darwin.go
+++ vulkan_darwin.go
@@ -3,8 +3,8 @@
 package vulkan
 
 /*
-#cgo darwin CFLAGS: -DVK_USE_PLATFORM_MACOS_MVK -Wno-deprecated-declarations
-#cgo darwin LDFLAGS: -Wl,-rpath,/usr/local/lib -F/Library/Frameworks -framework Cocoa -framework IOKit -framework IOSurface -framework QuartzCore -framework Metal -lMoltenVK -lc++
+#cgo darwin CFLAGS: -DVK_USE_PLATFORM_METAL_EXT -Wno-deprecated-declarations
+#cgo darwin LDFLAGS: -Wl,-rpath,/usr/local/lib -F/Library/Frameworks -framework Cocoa -framework IOKit -framework IOSurface -framework QuartzCore -framework Metal -lvulkan
 
 #include "headers/vulkan.h"
 #include "vk_wrapper.h"
@@ -13,12 +13,12 @@ package vulkan
 import "C"
 
 const (
-	// UsePlatformMacos means enabled support of MoltenVK.
+	// UsePlatformMacos means enabled support of Vulkan on macOS via Metal.
 	UsePlatformMacos = 1
-	// MvkMacosSurface means that VK_MVK_macos_surface is available.
-	MvkMacosSurface = 1
-	// MvkMacosSurfaceSpecVersion
-	MvkMacosSurfaceSpecVersion = 1
-	// MvkMacosSurfaceExtensionName
-	MvkMacosSurfaceExtensionName = "VK_MVK_macos_surface"
+	// ExtMetalSurface means that VK_EXT_metal_surface is available.
+	ExtMetalSurface = 1
+	// ExtMetalSurfaceSpecVersion
+	ExtMetalSurfaceSpecVersion = 1
+	// ExtMetalSurfaceExtensionName
+	ExtMetalSurfaceExtensionName = "VK_EXT_metal_surface"
 )
